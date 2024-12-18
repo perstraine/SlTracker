fa4bd8a35c20b04576b21deec6c0aff790133e15
perstraine <41206120+perstraine@users.noreply.github.com>
2024-12-18 13:49:00 +1300

    Pokemon api interaction and caching initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css'
+import { Pokedex } from 'pokeapi-js-wrapper';
+const P = new Pokedex();
 
 function App() {
-
+  const [wew, setWew] = useState("");
   return (
     <>
-      <h1>Dev Branch Init</h1>
+      <input onChange={(e)=>{setWew(e.target.value)}}></input>
+      <button onClick={()=>P.getPokemonByName(wew).then(function (response) {
+    console.log(response);
+      })}>
+        find
+      </button>
+      <p>{wew}</p>
     </>
   )
 }
